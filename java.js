1433a56c08259cae9a77fe5ff4c9d64d914445c2
Ayed49 <ayedbabu60@gmail.com>
2024-01-21 18:07:39 +0600

    first commit

--- java.js
+++ java.js
@@ -10,3 +10,31 @@ document.getElementById('login-submit').addEventListener('click', function(){
         window.location.href = 'bank.html';
     }
 })
+
+// handle deposit button event
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
+    // console.log(depositAmount);
+
+
+    // update deposit total
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = parseFloat (previousDepositAmount) + newDepositAmount;
+    depositTotal.innerText = newDepositTotal;
+
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+
+    // clear the deposit input fild
+    depositInput.value = '';
+})
