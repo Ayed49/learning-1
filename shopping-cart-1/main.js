2483cb06d57d32c63aee92094ec68ecf94b668c4
Ayed49 <146350329+Ayed49@users.noreply.github.com>
2024-02-23 22:29:58 +0600

    Add files via upload

---
+++ shopping-cart-1/main.js
@@ -0,0 +1,31 @@
+function updateCaseNumber(isIncreasing){
+    const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+    if(isIncreasing == true){
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0){
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // update case-total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    // const caseInput = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    // caseInput.value = parseInt(caseNumber) + 1;
+    updateCaseNumber(true);
+    
+});
+document.getElementById('case-minus').addEventListener('click', function (){
+    // const caseInput = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    updateCaseNumber(false);
+    // caseInput.value = parseInt(caseNumber) - 1;
+})
